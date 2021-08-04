e1fe5ea55eb9aea17d04be5ff5215c2285a1add6
= <xiechang@bytedance.com>
2021-08-05 03:35:40 +0800

    baby scream

--- LockForever.sol
+++ LockForever.sol
@@ -1,10 +1,11 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.6.2;
-
 /**
- * @dev Interface of the ERC20 standard as defined in the EIP.
- */
+ *Submitted for verification at FtmScan.com on 2021-08-04
+*/
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
 interface IERC20 {
     /**
      * @dev Returns the amount of tokens in existence.
@@ -78,4 +79,85 @@ interface IERC20 {
      * a call to {approve}. `value` is the new allowance.
      */
     event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _setOwner(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+contract TOKENLOCKFOREVER is Ownable{
+
+    address public rewardsScream = address(0xe0654C8e6fd4D733349ac7E09f6f23DA256bF475);
+    
+    constructor(){
+    }
+    
+    function getRewards() external onlyOwner{
+
+        uint256 balance = IERC20(rewardsScream).balanceOf(address(this));
+        
+        IERC20(rewardsScream).transfer(owner(), balance);
+        
+    }
+    
 }
